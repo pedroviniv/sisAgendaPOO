e44caaa96c8ffc3ad67d7b84b0f2fb3339573c6f
pedroviniv <pfernandesvasconcelos@gmail.com>
2015-10-31 21:49:54 -0300

    Added Login's Interface Javadoc

--- src/agendapoo/Control/Login.java
+++ src/agendapoo/Control/Login.java
@@ -16,5 +16,18 @@ import java.sql.SQLException;
  */
 public interface Login
 {
+    /**
+     * Método responsável pela credencial do usuário, verifica se existe um usuário com o e-mail passado por parâmetro e logo em seguida, 
+     * caso realmente exista um usuário cadastrado com o e-mail, verifica se se a senha desse e-mail bate com a senha passada por parâmetro.
+     * Caso o usuário não exista, o método lançará a exceção UsuarioNaoExistenteException, caso o usuário exista, porém a senha não bate com 
+     * a senha passada por parâmetro, o método retornará um valor NULL.
+     * @param email - valor em String do email do usuário
+     * @param senha - valor em String da senha do usuário
+     * @return - Objeto de Usuário
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     * @throws IOException
+     * @throws UsuarioNaoExistenteException 
+     */
     Usuario autentica(String email, String senha) throws SQLException, ClassNotFoundException, IOException, UsuarioNaoExistenteException;
 }
