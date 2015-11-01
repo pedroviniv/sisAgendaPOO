ca292e1f5bc931e45d98372f0267f8e01e0ec792
pedroviniv <pfernandesvasconcelos@gmail.com>
2015-10-31 22:26:59 -0300

    Added ControlSistema's Javadoc

--- src/agendapoo/Control/ControlSistema.java
+++ src/agendapoo/Control/ControlSistema.java
@@ -11,7 +11,9 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 /**
- *
+ * Classe responsável pelo Login do usuário no sistema.
+ * login - Interface com o método de autenticação de Usuário
+ * loggedUser - Usuario logado no sistema.
  * @author kieckegard
  */
 public class ControlSistema
@@ -23,6 +25,17 @@ public class ControlSistema
         login = new ControlUsuario();
     }
 
+    /**
+     * Método responsável por autenticar e retornar um objeto de usuário de acordo com as credenciais informadas (email e senha).
+     * @param email - String contendo o e-mail especificado
+     * @param senha - String contendo a senha especificado
+     * @return Objeto de Usuario, caso encontre um usuário e a senha passada por parâmetro seja referente ao usuário encontrado, Null caso 
+     * encontre o usuário porém a senha não bata com a senha do usuário encontrado.
+     * @throws SQLException
+     * @throws IOException
+     * @throws ClassNotFoundException
+     * @throws UsuarioNaoExistenteException - Exceção lançada caso não encontre um usuário com o e-mail especificado.
+     */
     public Usuario logIn(String email, String senha) throws SQLException, IOException, ClassNotFoundException, UsuarioNaoExistenteException
     {
         Usuario u = login.autentica(email, senha);
@@ -30,6 +43,9 @@ public class ControlSistema
         return u;
     }
     
+    /**
+     * @return Usuario logado no sistema.
+     */
     public Usuario getLoggedUser(){
         return loggedUser;
     }
