f3065141acdfb3e30d34abd3024805d4c8af98a9
pedroviniv <pfernandesvasconcelos@gmail.com>
2015-10-31 22:22:02 -0300

    Added ControlAtividade's Javadoc

--- src/agendapoo/Control/ControlAtividade.java
+++ src/agendapoo/Control/ControlAtividade.java
@@ -25,7 +25,10 @@ import java.util.List;
 import org.apache.commons.mail.EmailException;
 
 /**
- *
+ * Classe que terá a implementação de todos os métodos de AtividadeController e mais alguns que se farão necessário para
+ * operações com Atividade.
+ * EmailSender - Interface com o método de enviar Email
+ * IAtividadeDAO - Interface com os métodos para manipulação de Atividades no banco de dados ou arquivo
  * @author kieckegard
  */
 public class ControlAtividade implements AtividadeController
@@ -39,6 +42,23 @@ public class ControlAtividade implements AtividadeController
     }
     
     
+    /**
+     * Método responsável por construir um objeto de Atividade e logo em seguida, Cadastra-lo no banco, seja usando arquivo ou um sgbd.
+     * @param descricao - A descrição da atividade
+     * @param local - O local em que a atividade será realizada
+     * @param data - A data que a atividade será realizada
+     * @param horaInicio - O horário que a atividade começa.
+     * @param horaFim - O horário que a atividade termina.
+     * @param convidados - Os convidados que participarão da atividade e serão notificados por e-mail sobre a atividade.
+     * @param tipo - O tipo da atividade, podendo ser PROFISSIONAL, ACADEMICO ou PESSOAL.
+     * @param u - O Usuário que está cadastrando a atividade.
+     * @throws SQLException
+     * @throws IOException
+     * @throws ClassNotFoundException
+     * @throws InvalidTimeRangeException - A exceção é lançada caso o horário inicial da atividade seja maior que o horário final.
+     * @throws TimeInterferenceException - A exceção é lançado caso o horário da atividade entre em conflito com outras atividades cadastradas.
+     * @throws EmailException - A exceção é lançada caso não consiga enviar os e-mails para os convidados da atividade.
+     */
     @Override
     public void cadastraAtividade(String descricao, String local, String data, String horaInicio, String horaFim, List<String> convidados, TipoAtividade tipo, Usuario u) throws SQLException, IOException, ClassNotFoundException, TimeInterferenceException, InvalidTimeRangeException, EmailException
     {
@@ -48,7 +68,7 @@ public class ControlAtividade implements AtividadeController
         LocalTime end = LocalTime.parse(horaFim);
         if(start.compareTo(end)==0)
             throw new TimeInterferenceException("Ué, sua atividade vai durar 0 minutos? Por favor, ajeita isso!");
-        if(isTimeRangeValid(start,end))
+        if(!isTimeRangeValid(start,end))
             throw new InvalidTimeRangeException("Ué, sua atividade vai durar um tempo negativo? Por favor, ajeita isso!");
         if(isValidTimeByUser(data_aux,start,end,u)){
             Atividade a = new Atividade(descricao,local,data_aux,start,end,tipo,u);
@@ -59,6 +79,14 @@ public class ControlAtividade implements AtividadeController
         }else throw new TimeInterferenceException("Houve um choque de horário com outras atividades cadastradas. \nPor favor, selecione um outro horário!");
     }
 
+    /**
+     * Remove a atividade passada por parâmetro do Banco de dados ou arquivo.
+     * @param atividade - Objeto de Atividade
+     * @throws SQLException
+     * @throws IOException
+     * @throws ClassNotFoundException
+     * @throws EmailException - A exceção é lançada caso o controlador não consiga enviar a mensagem aos convidados avisando sobre a remorção da atividade.
+     */
     @Override
     public void deletaAtividade(Atividade atividade) throws SQLException, IOException, ClassNotFoundException, EmailException
     {
@@ -67,12 +95,22 @@ public class ControlAtividade implements AtividadeController
             sender.sendEmail(atividade, atividade.getUsuario(),TipoEmail.DELETE);
     }
 
+    /**
+     * Atualiza os atributos de atividade no banco de dados ou arquivo.
+     * @param atividade - Objeto de Atividade
+     * @throws SQLException
+     * @throws IOException
+     * @throws ClassNotFoundException
+     * @throws InvalidTimeRangeException - A exceção é lançada caso o horário inicial da atividade seja maior que o horário final.
+     * @throws TimeInterferenceException - A exceção é lançado caso o horário da atividade entre em conflito com outras atividades cadastradas.
+     * @throws EmailException - A exceção é lançada caso não consiga enviar os e-mails para os convidados avisando sobre a atualização da atividade.
+     */
     @Override
     public void atualizaAtividade(Atividade atividade) throws SQLException, IOException, ClassNotFoundException, TimeInterferenceException, EmailException, InvalidTimeRangeException
     {
         if(atividade.getHoraInicio().compareTo(atividade.getHoraFim())==0)
             throw new TimeInterferenceException("Ué, sua atividade vai durar 0 minutos? Por favor, ajeita isso!");
-        if(isTimeRangeValid(atividade.getHoraInicio(),atividade.getHoraFim()))
+        if(!isTimeRangeValid(atividade.getHoraInicio(),atividade.getHoraFim()))
             throw new InvalidTimeRangeException("Ué, sua atividade vai durar um tempo negativo? Por favor, ajeita isso!");
         if(isValidTimeUpdate(atividade)){
             dao.update(atividade);
@@ -81,6 +119,16 @@ public class ControlAtividade implements AtividadeController
         }else throw new TimeInterferenceException("Houve um choque de horário com outras atividades cadastradas. \nPor favor, selecione um outro horário!");
     }
     
+    /**
+     * Verifica se o horário inserido após uma atualização é válido, ou seja, se entra em conflito
+     * com outras atividades, caso entre em conflito com outras atividades retornará False, caso contrário,
+     * True, Como é uma atualização nesse método a verificação ignorará o horário que tiver no banco dessa mesma atividade.
+     * @param current - Atividade que foi atualizada e que terá seu horário verificado.
+     * @return - valor booleano indicando se o horário é válido ou não.
+     * @throws SQLException
+     * @throws IOException
+     * @throws ClassNotFoundException 
+     */
     private boolean isValidTimeUpdate(Atividade current) throws SQLException, IOException, ClassNotFoundException{
         boolean isValid = false;
         List<Atividade> atividades = dao.list(current.getUsuario());
@@ -110,10 +158,32 @@ public class ControlAtividade implements AtividadeController
         return true;
     }
     
+    /**
+     * Verifica se o horário inserido após uma inserção é válido, ou seja, se entra em conflito
+     * com outras atividades, caso entre em conflito com outras atividades retornará False, caso contrário,
+     * @param start - LocalTime contendo o horário inicial da atividade
+     * @param end - LocalTime contendo o horário final da atividade
+     * @return - valor booleano indicando se o horário inicial é maior ou não que o horário final.
+     * @throws InvalidTimeRangeException 
+     */
     private boolean isTimeRangeValid(LocalTime start, LocalTime end) throws InvalidTimeRangeException{
-        return start.isAfter(end);
+        return !start.isAfter(end);
     }
     
+    /**
+      * Verifica se o horário inserido após uma inserção é válido, ou seja, se entra em conflito
+     *  com outras atividades, caso entre em conflito com outras atividades retornará False, caso contrário,
+     * retornará True.
+     * @param data - LocalDate contendo a data da atividade
+     * @param start - LocalTime contendo o horário inicial da atividade
+     * @param end - LocalTime contendo o horário final da atividade
+     * @param u - Usuário que está adicionando a atividade (faz-se necessário por verificar se o horário é válido apenas considerando as atividades
+     * daquele usuário)
+     * @return - valor booleano indicando se o horário definido para atividade é válido ou não.
+     * @throws SQLException
+     * @throws IOException
+     * @throws ClassNotFoundException 
+     */
     private boolean isValidTimeByUser(LocalDate data, LocalTime start, LocalTime end, Usuario u) throws SQLException, IOException, ClassNotFoundException{
         boolean isValid = false;
         List<Atividade> atividades = dao.list(u);
@@ -141,8 +211,18 @@ public class ControlAtividade implements AtividadeController
         return true;
     }
     
-    public Atividade getAtividadeById(Usuario u, String id) throws SQLException, IOException, ClassNotFoundException{
-        List<Atividade> atividades = dao.list(u);
+    /**
+     * Procura uma atividade no banco de dados ou arquivo que contenha a Id passada por parâmetro, o usuário faz-se necessário para poder
+     * filtrar logo pelas atividades do usuário e em seguida procurar na lista de atividades do usuário a Id passada por parâmetro.
+     * @param usuario - usuario logado no sistema
+     * @param id - identificação da atividade
+     * @return - Objeto de Atividade ou nulo, caso não encontre nenhuma atividade com o Id especificado.
+     * @throws SQLException
+     * @throws IOException
+     * @throws ClassNotFoundException 
+     */
+    public Atividade getAtividadeById(Usuario usuario, String id) throws SQLException, IOException, ClassNotFoundException{
+        List<Atividade> atividades = dao.list(usuario);
         for(Atividade a : atividades){
             if(a.getId().equals(id))
                 return a;
@@ -150,6 +230,14 @@ public class ControlAtividade implements AtividadeController
         return null;
     }
     
+     /**
+     * Retorna uma Lista contendo todas as atividades cadastradas no banco de dados ou arquivo referente ao Usuário passado por parâmetro.
+     * @param u
+     * @return Lista de Objetos Atividade
+     * @throws SQLException
+     * @throws IOException
+     * @throws ClassNotFoundException 
+     */
     @Override
     public List<Atividade> getAtividadesByUser(Usuario u) throws SQLException, IOException, ClassNotFoundException{
         List<Atividade> list = dao.list(u);
